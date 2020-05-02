9f9bc556e694bf257c319aa1a2252e6dcf0a8120
aliensmart <kaoua17@gmail.com>
2020-05-02 02:13:26 -0400

    added files in the app template

--- app/views.py
+++ app/views.py
@@ -4,10 +4,10 @@ from django.views.generic import View, TemplateView
 
 # Create your views here.
 
-class IndexView(TemplateView):
-    template_name = 'app/index.html'
+# class IndexView(TemplateView):
+#     template_name = 'app/index.html'
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injection'] = 'Basic injection'
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['injection'] = 'Basic injection'
+#         return context
